2fe46f28a240b56f11783a0550e2b6b10a8fb62a
Dmitrii Pashutskii <dpashutskii@pm.me>
2020-07-03 12:55:02 +0800

    add tailwind, adjusted undex page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#034694",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
