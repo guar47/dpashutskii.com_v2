6155dca4b8cf9435313296bfebc91375b8416b0a
Dmitrii Pashutskii <dpashutskii@gmail.com>
2020-09-26 17:57:15 +0800

    add confirm page

---
+++ src/pages/confirm.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class Confirm extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="Confirm subscription" />
+        <h1>The last step...</h1>
+        <p>
+          Thank you for subscribing. You will need to check your inbox and
+          confirm your subscription.
+        </p>
+      </Layout>
+    )
+  }
+}
+
+export default Confirm
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
