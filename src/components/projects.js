c30ca423dd0e63d0d17ebcce314ebd21ff17e6b1
Dmitrii Pashutskii <dpashutskii@pm.me>
2021-05-13 17:02:27 +0300

    improved project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,58 +1,173 @@
 import React from "react"
+import Image from "gatsby-image"
+import NodeJsIcon from "../../content/assets/icons/nodejs-icon.svg"
+import MongoDbIcon from "../../content/assets/icons/mongodb-icon.svg"
+import ExpressIcon from "../../content/assets/icons/express-icon.svg"
+import ReactIcon from "../../content/assets/icons/react-icon.svg"
+import JavascriptIcon from "../../content/assets/icons/javascript-icon.svg"
+import RubyIcon from "../../content/assets/icons/ruby-icon.svg"
+import RailsIcon from "../../content/assets/icons/rails-icon.svg"
+import PostgresIcon from "../../content/assets/icons/postgresql-icon.svg"
+import PythonIcon from "../../content/assets/icons/python-icon.svg"
+import DjangoIcon from "../../content/assets/icons/django-icon.svg"
 
 import ProjectCard from "./project-card"
 
 const Projects = () => {
   return (
     <div className="mb-12">
-      <h2 className="text-2xl mt-0 mb-3 font-bold">My projects:</h2>
-      <h3 className="text-xl mt-0 mb-3 font-bold">Tech:</h3>
-      <div className="grid grid-col-1 gap-4">
-        <ProjectCard
-          title="Arcade Hub"
-          description="Apple Arcade games aggregator."
-          launchDate="2020-03-17"
-          url="https://arcade-hub.com"
-          stack={true}
-        />
-        <ProjectCard
-          title="Learning Story"
-          description="Community and goal achieving platform that encourages people to
-          share and discuss their learnings in public."
-          launchDate={"2020-12-15"}
-          url="https://getlearningstory.com"
-          stack={true}
-        />
-        <ProjectCard
-          title="Nextplace"
-          description="Website about visas for digital nomads, financial independant and other long-term expats"
-          launchDate={"2021-03-10"}
-          url="https://nextplaceto.com"
-          stack={true}
-        />
-        <h3 className="text-xl mt-5 mb-0 font-bold">Non tech:</h3>
-        <ProjectCard
-          title="This Blog"
-          description="Blog about indie making, development, traveling and nomading."
-          launchDate="2018-07-21"
-          url="https://dpashutskii.com"
-          stack={false}
-        />
-        <ProjectCard
-          title="YouTube channel"
-          description="My channel about traveling, nomading, tech and related experiences."
-          launchDate="2018-08-30"
-          url="https://www.youtube.com/channel/UCtZd4Chy6nqjX1X06ypzEgw"
-          stack={false}
-        />
-        <ProjectCard
-          title="AnotherWayOut"
-          description="Telegram channel in Russian about traveling, immigration, remote work and nomad lifestyle."
-          launchDate="2019-04-24"
-          url="https://t.me/another_way_out"
-          stack={false}
-        />
-      </div>
+      <h2 className="text-2xl mb-7 mt-0 font-black">my projects:</h2>
+
+      <article className="mb-7 font-headers border-b border-black pb-2">
+        <h3 className="mt-2 mb-0">
+          <a
+            className="font-semibold shadow-none text-black hover:text-blue-800"
+            href="https://nextplaceto.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            nextplace.
+          </a>
+        </h3>
+        <div className="text-black">
+          <span className="">
+            website about visas for digital nomads, financial independant and
+            other long-term expats.
+          </span>
+        </div>
+        <div className="text-black">
+          <span className="font-semibold">launched:</span> 10.03.2021
+        </div>
+        <div className="text-black">
+          <span className="font-semibold mr-2">stack v1:</span>
+          python, django, postgresql, javascript
+        </div>
+        <div className="text-black">
+          <span className="font-semibold mr-2">stack v2:</span>
+          javascript, next.js, posgtgresql, strapi
+        </div>
+      </article>
+
+      <article className="mb-7 font-headers border-b border-black pb-2">
+        <h3 className="mt-2 mb-0">
+          <a
+            className="font-semibold shadow-none text-black hover:text-blue-800"
+            href="https://getlearningstory.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            learning story.
+          </a>
+        </h3>
+        <div className="text-black">
+          <span className="">
+            community and goal achieving platform that encourages people to
+            share and discuss their learnings in public.
+          </span>
+        </div>
+        <div className="text-black">
+          <span className="font-semibold">launched:</span> 15.12.2020
+        </div>
+        <div className="text-black">
+          <span className="font-semibold mr-2">stack:</span>
+          ruby, ruby on rails, postgresql, javascript
+        </div>
+      </article>
+
+
+      <article className="mb-7 font-headers border-b border-black pb-2">
+        <h3 className="mt-2 mb-0">
+          <a
+            className="font-semibold shadow-none text-black hover:text-blue-800"
+            href="https://arcade-hub.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            arcade-hub.
+          </a>
+        </h3>
+        <div className="text-black">
+          <span className="">apple arcade games aggregator.</span>
+        </div>
+        <div className="text-black">
+          <span className="font-semibold">launched:</span> 17.03.2020
+        </div>
+        <div className="text-black">
+          <span className="font-semibold mr-2">stack:</span>
+          javascript, react.js, node.js
+        </div>
+      </article>
+
+
+      <article className="mb-7 font-headers border-b border-black pb-2">
+        <h3 className="mt-2 mb-0">
+          <a
+            className="font-semibold shadow-none text-black hover:text-blue-800"
+            href="https://t.me/another_way_out"
+            target="_blank"
+            rel="noreferrer"
+          >
+            another way out.
+          </a>
+        </h3>
+        <div className="text-black">
+          <span className="">
+            telegram channel in Russian about traveling, immigration, remote
+            work and nomad lifestyle.
+          </span>
+        </div>
+        <div className="text-black">
+          <span className="font-semibold">launched:</span> 24.04.2019
+        </div>
+      </article>
+
+
+      <article className="mb-7 font-headers border-b border-black pb-2">
+        <h3 className="mt-2 mb-0">
+          <a
+            className="font-semibold shadow-none text-black hover:text-blue-800"
+            href="https://www.youtube.com/channel/UCtZd4Chy6nqjX1X06ypzEgw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            youtube channel.
+          </a>
+        </h3>
+        <div className="text-black">
+          <span className="">
+            my channel about traveling, nomading, tech and related experiences
+          </span>
+        </div>
+        <div className="text-black">
+          <span className="font-semibold">launched:</span> 30.08.2018
+        </div>
+      </article>
+
+
+      <article className="mb-7 font-headers border-b border-black pb-2">
+        <h3 className="mt-2 mb-0">
+          <a
+            className="font-semibold shadow-none text-black hover:text-blue-800"
+            href="https://dpashutskii.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            blog.
+          </a>
+        </h3>
+        <div className="text-black">
+          <span className="">
+            this blog about indie making, development, traveling and nomading.
+          </span>
+        </div>
+        <div className="text-black">
+          <span className="font-semibold">launched:</span> 21.07.2018
+        </div>
+        <div className="text-black">
+          <span className="font-semibold mr-2">stack:</span>
+          javascript, gatsbyjs, markdown
+        </div>
+      </article>
     </div>
   )
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,27 +11,6 @@ class Blog extends React.Component {
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
 
-    const textClass = {
-      making: "text-indigo-800",
-      personal: "text-gray-800",
-      dev: "text-yellow-800",
-      travel: "text-green-800"
-    }
-
-    const bgClass = {
-      making: "bg-indigo-100",
-      personal: "bg-gray-100",
-      dev: "bg-yellow-100",
-      travel: "bg-green-100"
-    }
-
-    const borderClass = {
-      making: "border-indigo-400",
-      personal: "border-gray-400",
-      dev: "border-yellow-400",
-      travel: "border-green-400"
-    }
-
     return (
       <Layout
         location={this.props.location}
@@ -39,45 +18,21 @@ class Blog extends React.Component {
         lang={pageContext.langKey}
       >
         <SEO title="Side projects list" />
-        <h2 className="text-xl mt-0 mb-5 font-bold">Blog posts:</h2>
+        <h2 className="text-2xl mb-7 mt-0 font-black">blog posts:</h2>
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
-            <article
-              className="mb-5 sm:pb-4 mt-4 border-b flex flex-col sm:flex-row"
-              key={node.fields.slug}
-            >
-              {node.frontmatter.image && (
-                <Link
-                  className="shadow-none sm:w-1/3 sm:flex-shrink-0"
-                  to={`${node.fields.slug}`}
-                >
-                  <Img
-                    fluid={node.frontmatter.image.childImageSharp.fluid}
-                    backgroundColor={`#040e18`}
-                    className="h-full sm:w-full rounded-sm hover:opacity-70"
-                  />
-                </Link>
-              )}
-              <header className="mt-3 sm:ml-5">
-                <h3 className="my-0">
+            <article className="mb-7 font-headers" key={node.fields.slug}>
+              <header>
+                <h3 className="mt-2 mb-0">
                   <Link
-                    className="text-md font-bold shadow-none font-text text-gray-800 hover:text-blue-800"
+                    className="text-lg font-semibold shadow-none text-black hover:text-blue-800"
                     to={`${node.fields.slug}`}
                   >
                     {title}
                   </Link>
                 </h3>
-                <span
-                  className={`mr-3 px-2 inline-flex text-xs leading-5 font-semibold rounded-sm border
-                          ${borderClass[node.frontmatter.category]}
-                          ${bgClass[node.frontmatter.category]}
-                          ${textClass[node.frontmatter.category]}`}
-                >
-                  {node.frontmatter.category}
-                </span>
                 <small>{node.frontmatter.date}</small>
-                <p className="mt-3">{node.frontmatter.description}</p>
               </header>
             </article>
           )
