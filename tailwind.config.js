8f185178bdf36c953ec171a639470a00e7f634af
Dmitrii Pashutskii <dpashutskii@gmail.com>
2022-02-11 11:17:43 +0400

    updagrade tailwind and other packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  content: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
         headers: ["Montserrat", "sans-serif"],
         text: ["Merriweather", "Georgia", "serif"],
       },
-      animation: ["hover", "focus"],
-    },
-  },
-  variants: {
-    extend: {
       animation: ["hover", "focus"],
     },
   },
